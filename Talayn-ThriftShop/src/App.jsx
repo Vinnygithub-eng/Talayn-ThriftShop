b41ba6b8a3191e22345c54149969b4b28d0c67dc
Bryan Vinyets <bryanestico@gmail.com>
2026-06-18 12:11:02 -0400

    Mostly Finished Demo

--- Talayn-ThriftShop/src/App.jsx
+++ Talayn-ThriftShop/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 import './App.css';
+
+import { FaInstagram, FaEbay, FaShopify } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
 import TagHolder1 from './assets/TalaynImages/TagHolder/handleOne.png';
 import street01 from './assets/TalaynImages/Jackets/Jacket01First.png';
 import street02 from './assets/TalaynImages/Jackets/Jacket01BrandAndSize.png';
@@ -18,6 +22,20 @@ import Shirt03 from './assets/TalaynImages/Shirts/PurpleSweatShirt.webp';
 import Shirt04 from './assets/TalaynImages/Shirts/TanSweater.webp';
 import Shirt05 from './assets/TalaynImages/Shirts/YeezusShirt.webp';
 import OwnerPhoto from './assets/TalaynImages/TagHolder/handleTwo.png';
+import Shorts01 from './assets/TalaynImages/Shorts/GrayJorts.webp';
+import Shorts02 from './assets/TalaynImages/Shorts/GreyShorts.webp';
+import Pants01 from './assets/TalaynImages/Pants/BlackSkinnyJeans.webp';
+import Pants02 from './assets/TalaynImages/Pants/BlueJeansRippedJeans.webp';
+import Pants03 from './assets/TalaynImages/Pants/BlueJeansWithEagleOnBack.webp';
+import Pants04 from './assets/TalaynImages/Pants/BrownSkinnyJeans.webp';
+import Pants05 from './assets/TalaynImages/Pants/GrayJoggers.webp';
+import Pants06 from './assets/TalaynImages/Pants/GreySkinnyJeans.webp';
+import Pants07 from './assets/TalaynImages/Pants/LegendPants.webp';
+import Pants08 from './assets/TalaynImages/Pants/LightBrownPants02.webp';
+import Pants09 from './assets/TalaynImages/Pants/Pants01First.png';
+import Pants10 from './assets/TalaynImages/Pants/StrippedWhiteLineBlueJoggers.webp';
+
+
 function ImageCarousel({ title, images}) {
 
   const [index, setIndex] = useState(0);
@@ -119,11 +137,48 @@ function FullscreenGallery({ title, images, onClose }) {
   );
 }
 
+function AboutPage({ onBack }){
+  return(
+    <section className="about-page">
+      <div className="about-page-card">
+        <div className="about-page-image">
+          <img
+          src="https://placehold.co/600x750?text=Owner+Photo"
+          alt="Owner palceholder"
+          />
+        </div>
+          <div className="about-page-text">
+            <h1>About <span>Talayn Thrift Shop </span> </h1>
+
+            <p>
+              This is a placeholder about section. Later, the owner can replace this
+              text with their real story, brand, mission, and what makes their thrift 
+              shop different.
+            </p>
+            <p>
+              Add details like how the pieces are found, cleaned, styled, and 
+              prepared before being posted for customers.
+            </p>
+          </div>
+        </div>
+
+        <button className="back-to-main" onClick={onBack}>
+          Back To Main Site
+        </button>
+    </section>
+  );
+}
+
 function App() {
   const streetwearImages= [street01, street02];
   const flannelImages = [Flannels01, Flannels02, Flannels03, Flannels04];
   const jerseysImages = [Jerseys01, Jerseys02, Jerseys03, Jerseys04];
   const shirtsImages = [Shirt01, Shirt02, Shirt03, Shirt04, Shirt05];
+  const ShortsImages = [Shorts01, Shorts02];
+  const PantsImages = [Pants01, Pants02, Pants03, Pants04, Pants05, Pants06, Pants07, Pants08, Pants09, Pants10];
+
+  //for our about page
+  const [showAboutPage, SetShowAboutPage] = useState(false);
 
   // Add this right here
   const [openGallery, setOpenGallery] = useState(null);
@@ -136,7 +191,13 @@ function App() {
     Flannels: flannelImages,
     Jerseys: jerseysImages,
     Shirts: shirtsImages,
+    Shorts: ShortsImages,
+    Pants: PantsImages
   };
+
+  if(showAboutPage) {
+    return<AboutPage onBack={() => SetShowAboutPage(false)} />
+  }
 
   const scrollToSection = (id) => {
     document.getElementById(id)?.scrollIntoView({
@@ -149,10 +210,14 @@ function App() {
     <>
       <section id="center">
         <div className="hero-overlay">
-          <h1 className="hero-title">TALAYN THRIFT SHOP</h1>
+          <h1 className="hero-title">TALAYN <span>THRIFT SHOP</span></h1>
+
           <p className="small-tag">RARE FINDS | STREETWEAR | VINTAGE</p>
 
-          <h1>HUNTED. CLEANED. CURATED.</h1>
+          <h1 className="hero-slogan">HUNTED
+          <span>CLEANED</span> 
+          <span>CURATED</span>
+          </h1>
 
           <p className="hero-description">
             Rare pieces, vintage heat, and second-life fashion drops.
@@ -181,6 +246,8 @@ function App() {
             <ImageCarousel title="Jerseys" images={ jerseysImages } />
              <ImageCarousel title="Flannels" images={ flannelImages} /> 
              <ImageCarousel title="Shirts" images={ shirtsImages } />
+             <ImageCarousel title="Shorts" images={ ShortsImages } />
+             <ImageCarousel title="Pants" images={ PantsImages } />
     
           </div>
 
@@ -235,7 +302,10 @@ function App() {
               to give quality fashion a second life.
             </p>
 
-            <button>LEARN MORE</button>
+            {/* <button>LEARN MORE</button> */}
+            <button onClick={() => SetShowAboutPage(true)}>
+              Learn More
+            </button>
           </div>
         </div>
       </section>
@@ -248,10 +318,23 @@ function App() {
         <p>Follow us on social media for the latest updates!</p>
 
         <div className="social-links">
-          <a href="#">INSTAGRAM</a>
-          <a href="#">Ebay</a>
-          <a href="#">Shopify</a>
-          <a href="#">EMAIL</a>
+         {/* Replace the # with the client's real Instagram URL */}
+         <a href="#" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+          Instagram
+         </a>
+
+          {/* Replace the # with the client's real Shopify URL */}
+         <a href="#" target="_blank" rel="noopener noreferrer">
+          <FaShopify />
+          Shopify
+         </a>
+         
+         {/* Replace the # with the client's real Email URL */}
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <MdEmail />
+            Email
+          </a>
         </div>
       </section>
 
